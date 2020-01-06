014f8af0fe9e5c114c0a0a7bacdeb5b14f29c83b
Chet Husk <chusk3@gmail.com>
2020-01-06 07:52:47 -0600

    revert YR_RULE fixed offsets

--- Libraries/dnYara.Interop/Interops/Types/YR_RULE.cs
+++ Libraries/dnYara.Interop/Interops/Types/YR_RULE.cs
@@ -3,48 +3,40 @@ using System.Runtime.InteropServices;
 
 namespace dnYara.Interop
 {
-    [StructLayout(LayoutKind.Explicit, Size = 448)]
-    public unsafe struct YR_RULE
+    [StructLayout(LayoutKind.Sequential)]
+    public struct YR_RULE
     {
         // Global flags
-        [FieldOffset(0)]
         public int g_flags;
 
         // Thread-specific flags
-        [FieldOffset(4)]
-        public fixed int t_flags[32];
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = Constants.YR_MAX_THREADS, ArraySubType = UnmanagedType.I4)]
+        public int[] t_flags;
 
-        [FieldOffset(136)]
         public IntPtr identifier;
 
-        [FieldOffset(144)]
         public IntPtr tags;
 
-        [FieldOffset(152)]
         public IntPtr metas;
 
-        [FieldOffset(160)]
         public IntPtr strings;
 
-        [FieldOffset(168)]
         public IntPtr ns;
 
         // Number of atoms generated for this rule.
-        [FieldOffset(176)]
         public int num_atoms;
 
         // Used only when PROFILING_ENABLED is defined. This is the sum of all values
         // in time_cost_per_thread. This is updated once on each call to
         // yr_scanner_scan_xxx.
-        [FieldOffset(184)]
         public long time_cost;
 
         // Used only when PROFILING_ENABLED is defined. This array holds the time
         // cost for each thread using this structure concurrenlty. This is necessary
         // because a global variable causes too much contention while trying to
         // increment in a synchronized way from multiple threads.
-        [FieldOffset(192)]
-        public fixed long time_cost_per_thread[32];
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = Constants.YR_MAX_THREADS, ArraySubType = UnmanagedType.I8)]
+        public long[] time_cost_per_thread;
     }
 
 }
