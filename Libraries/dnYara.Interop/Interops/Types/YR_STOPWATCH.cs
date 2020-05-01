a7c457453ee93a335db735a132ae2779b8f9a4e6
Chet Husk <chusk3@gmail.com>
2020-05-01 10:16:13 -0500

    use msbuild.sdk.extras to generate proper builds/packages for all RIDs

--- Libraries/dnYara.Interop/Interops/Types/YR_STOPWATCH.cs
+++ Libraries/dnYara.Interop/Interops/Types/YR_STOPWATCH.cs
@@ -4,14 +4,14 @@ using System.Runtime.InteropServices;
 namespace dnYara.Interop
 {
 
-#if win
+#if WIN
     [StructLayout(LayoutKind.Sequential)]
     public struct YR_STOPWATCH
     {
         public ulong frequency;
         public ulong start;
     }
-#elif osx
+#elif OSX
     [StructLayout(LayoutKind.Sequential)]
     public struct mach_timebase_info
     {
@@ -25,17 +25,17 @@ namespace dnYara.Interop
         public mach_timebase_info timebase;
         public ulong start;
     }
-#elif linux
+#elif LINUX
     [StructLayout(LayoutKind.Sequential)]
     public struct timeval {
-        public int64 tv_sec;
-        public int32 tc_usec;
+        public long tv_sec;
+        public int tc_usec;
     }
 
     [StructLayout(LayoutKind.Sequential)]
     public struct timespec {
-        public int64 tv_sec;
-        public int32 tc_nsec;
+        public long tv_sec;
+        public int tc_nsec;
     }
 
     [StructLayout(LayoutKind.Sequential)]
